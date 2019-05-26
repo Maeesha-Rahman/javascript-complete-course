c70d2cba26ce6cd3885a5c10bca2f22ff0aa802b
Maeesha <maeesha.rahman@gmail.com>
2019-05-26 13:01:44 -0400

    AJAX loading spinner

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -35,7 +35,7 @@
 
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 // global state of the app
     // search object - where we have search query and search results and this will be part of the state
@@ -57,11 +57,13 @@ const controlSearch = async () => {
         // 3. prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4. search for recipes
         await state.search.getResults(); // returns a promise
 
         // 5. render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -15,6 +15,38 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 }
 
+// 'pasta with tomato and spinach'
+// using the split method will split the string into an array with 5 elements [pasta, with, tomato, and, spinish];
+// since we now have an array, we can use the reduce method on it (accumulator, current)
+// initial value of accumulator is set to 0 and throughout the loops we add to it 
+
+/*
+// 'pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 0 + 5 = 5 // newTitle = ['pasta']
+acc: 5 // acc + cur.length = 5 + 4 = 9 // newTitle = ['pasta', 'with']
+acc: 9 // acc + cur.length = 9 + 6 = 15 // newTitle = ['pasta', 'with', 'tomato']
+acc: 15 // acc + cur.length = 15 + 3 = 18 // newTitle = ['pasta', 'with', 'tomato']
+acc: 18 // acc + cur.length = 18 + 7 = 25 // newTitle = ['pasta', 'with', 'tomato']
+*/
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            // return the new accumulator for the next iteration 
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        // join() will join elements of an array into a string separated by spaces (opposite of split method)
+        return `${newTitle.join(' ')} ...`;
+    } 
+    return title;
+}
+
 const renderRecipe = recipe => {
     const markup = `
     <li>
@@ -23,7 +55,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
